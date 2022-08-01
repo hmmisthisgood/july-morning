83747dece2727f555b50e22c749fbefc7cb6781f
test <test@test.com>
2022-08-01 07:59:26 +0545

    Oop day 1

---
+++ basics/oop/user.dart
@@ -0,0 +1,72 @@
+/// oop keywords:
+/// class , extends
+/// this , super ,with , override , abstract
+
+class User {
+  /// member variables
+
+  String username, bio, userType;
+  int age;
+
+  /// private
+  String _password;
+
+  User({
+    required this.age,
+    required this.userType,
+    required this.bio,
+    required this.username,
+    required String pass,
+  }) : this._password = pass;
+
+  // User(
+  //     {required String username,
+  //     required String userType,
+  //     required int age,
+  //     String? b}) {
+  //   this.username = username;
+  //   this.userType = userType;
+  // } // this.bio = b;
+
+  /// methods/functions
+
+  login() {
+    print("$username has logged in");
+    print(age);
+    print(userType);
+  }
+
+  logout() {}
+}
+
+// iheritence
+// abastraction
+// polymorphism
+// encapsulation
+
+main() {
+  User ram = User(
+      age: 11,
+      username: "iamRamey",
+      userType: "admin",
+      bio: "this",
+      pass: "strongpassword"); // defualt constructor
+
+  ram.username = "iamNotRamey";
+  ram.login();
+  // ram.logout();
+
+  User jack = User(
+      age: 11,
+      username: "iamJack",
+      userType: "admin",
+      bio: "this",
+      pass: "secretPassword");
+  jack.login();
+
+  // User mary = User();
+}
+
+
+/// create a class MobilePhone , add its properties, 
+/// create 3 phones, iphone13, note20, oneplusNord
---
+++ basics/oop/user_test.dart
@@ -0,0 +1,11 @@
+import 'user.dart';
+
+main() {
+  User notRam = User(
+    age: 11,
+    username: "iamRamey",
+    userType: "admin",
+    bio: "this",
+    pass: "password",
+  );
+}
